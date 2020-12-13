5933c98e1d5835a2c3817899b8efea33a5ecb17f
Filip Marijanovic <m.filip@live.com>
2020-12-13 14:41:09 +0100

    jQuery

---
+++ app.js
@@ -0,0 +1,23 @@
+//JQuery()
+$(document).ready(function(){
+    $("#div1").html("Hello from jQuery....");
+    $("button").click(() => {
+        //$("#div1").append('<button>Dodatno dugme</button>')
+        //$("#div1").hide()
+        //$("#div1").toggle();
+        //$("#div1").css('background-color', 'red');
+        //$("#div1").remove();
+    })
+
+    // $("button").on('mouseover', () => {
+    //     alert('Klik na dugme')
+    // })
+
+    //$("button").trigger('mouseover')
+    // $("li:not(.klasa1)")
+    // $("li:eq(5)").forEach(() => {
+
+    // });
+});
+
+
