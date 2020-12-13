7babd9611c0253822608813a896a9531eabf7261
Filip Marijanovic <m.filip@live.com>
2020-12-13 15:19:47 +0100

    AJAX

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 //JQuery()
 $(document).ready(function(){
-    $("#div1").html("Hello from jQuery....");
+    //$("#div1").html("Hello from jQuery....");
     $("button").click(() => {
         //$("#div1").append('<button>Dodatno dugme</button>')
         //$("#div1").hide()
         //$("#div1").toggle();
         //$("#div1").css('background-color', 'red');
         //$("#div1").remove();
+        callAPI();
     })
 
     // $("button").on('mouseover', () => {
@@ -20,4 +21,19 @@ $(document).ready(function(){
     // });
 });
 
+//AJAX
+
+function callAPI(){
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            let response_json = JSON.parse(this.responseText)
+            //console.log(response_json)
+            //$("#div1").html();
+            $("#div1").html(response_json.slip.advice)
+        }
+    }
+    xhttp.open("GET", "https://api.adviceslip.com/advice", true);
+    xhttp.send();
+};
 
